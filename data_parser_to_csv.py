932a586560a7b86438eb83a1d71249c61de4e320
theFSO <1104532109@qq.com>
2022-02-05 00:53:17 -0500

    fix parser error and formatting

--- data_parser_to_csv.py
+++ data_parser_to_csv.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from datetime import datetime
 
 def txt_to_csv(filename):
     f = open(filename)
@@ -11,18 +11,20 @@ def txt_to_csv(filename):
     for i in range(0,len(f_list)):
         temp = []
         if (i%3 == 0):
-            st.append(f_list[i])
+            tempdate = datetime.strptime(f_list[i], "%B %d, %Y").strftime("%Y/%m/%d")
+            st.append(tempdate)
         elif (i%3 == 1):
-            ed.append(f_list[i])
+            tempdate = datetime.strptime(f_list[i], "%B %d, %Y").strftime("%Y/%m/%d")
+            ed.append(tempdate)
         else:
             foo = f_list[i].split(" ")
-            per.append(foo[0] + foo[1])
-            num.append(foo[2])
-    filename_without_subfix = filename.split(".")[0]
-    df = pd.DataFrame({'start_date': st, 'end_data': ed, 'period': per, 'number':num})
+            per.append(foo[0][1:] + " " + foo[1][:-1])
+            num.append(foo[2].replace(",", ""))
+    filename_without_subfix = filename[:-4]
+    df = pd.DataFrame({'start_date': st, 'end_data': ed, 'period': per, 'number': num})
     df.to_csv(filename_without_subfix + ".csv", index=False, sep=',')
 
 
 if __name__ == "__main__":
-    txt_to_csv("data/CRSB_by_time.txt")
+    txt_to_csv("./data/CRSB_by_time.txt")
 
